624529ad5d163804cbc7a8d33e8c246eeea6911c
fi7730 <jjfillingham@me.com>
2014-08-18 11:40:59 +0100

    Fixed WiFi linker crash
    removed wifi linker because it wasn't working and was causing a crash

--- src/main/java/modmuss50/network/items/NetworkItems.java
+++ src/main/java/modmuss50/network/items/NetworkItems.java
@@ -5,7 +5,6 @@ import net.minecraft.item.Item;
 public class NetworkItems {
     public static Item tablet;
     public static Item serverCart;
-    public static Item wifiLinker;
     public static Item wifiGoggles;
     public static Item ItemBattery;
     public static Item parts;
